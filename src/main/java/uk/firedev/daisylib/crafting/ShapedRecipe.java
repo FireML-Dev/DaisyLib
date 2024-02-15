d12c84baed822a69a51fae2573b93eece8cc56a7
FireML <106587317+FireML@users.noreply.github.com>
2024-02-15 20:05:18 +0000

    Add ShapedRecipe Class

---
+++ src/main/java/uk/firedev/daisylib/crafting/ShapedRecipe.java
@@ -0,0 +1,77 @@
+package uk.firedev.daisylib.crafting;
+
+import org.bukkit.Bukkit;
+import org.bukkit.Material;
+import org.bukkit.NamespacedKey;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.RecipeChoice;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.List;
+
+public class ShapedRecipe extends org.bukkit.inventory.ShapedRecipe {
+
+    private final NamespacedKey key;
+    private final ItemStack result;
+    private final List<ItemStack> recipeItems;
+    private boolean registered = false;
+
+    public ShapedRecipe(@NotNull NamespacedKey key, @NotNull ItemStack result, @NotNull List<ItemStack> recipeItems) {
+        super(key, result);
+        this.key = key;
+        this.result = result;
+        if (recipeItems.size() > 9) {
+            this.recipeItems = recipeItems.subList(0, 8);
+        } else {
+            this.recipeItems = recipeItems;
+        }
+        buildRecipe();
+    }
+
+    public boolean isRegistered() { return registered; }
+
+    private void buildRecipe() {
+        shape("ABC", "DEF", "GHI");
+        for (int i = 0; i < 9; ++i) {
+            ItemStack item;
+            try {
+                item = recipeItems.get(i);
+            } catch (IndexOutOfBoundsException ex) {
+                item = new ItemStack(Material.AIR);
+            }
+            char loopChar = charFromInt(i);
+            setIngredient(loopChar, new RecipeChoice.ExactChoice(item));
+        }
+    }
+
+    private char charFromInt(int i) {
+        if (i > 8 || i < 0) {
+            return 'X';
+        }
+        char[] charMap = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I'};
+        return charMap[i];
+    }
+
+    public boolean register() {
+        if (registered) {
+            return false;
+        }
+        if (Bukkit.addRecipe(this)) {
+            registered = true;
+            return true;
+        }
+        return false;
+    }
+
+    public boolean unregister() {
+        if (!registered) {
+            return false;
+        }
+        if (Bukkit.removeRecipe(this.key, true)) {
+            registered = false;
+            return true;
+        }
+        return false;
+    }
+
+}
