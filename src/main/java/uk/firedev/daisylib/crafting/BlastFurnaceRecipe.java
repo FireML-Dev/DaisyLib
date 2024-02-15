b661f310f7f5eff1fa8705dfae2066dc42e657b0
FireML <106587317+FireML@users.noreply.github.com>
2024-02-15 20:11:58 +0000

    Add BlastFurnaceRecipe Class

---
+++ src/main/java/uk/firedev/daisylib/crafting/BlastFurnaceRecipe.java
@@ -0,0 +1,48 @@
+package uk.firedev.daisylib.crafting;
+
+import org.bukkit.Bukkit;
+import org.bukkit.Material;
+import org.bukkit.NamespacedKey;
+import org.bukkit.inventory.BlastingRecipe;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.RecipeChoice;
+import org.jetbrains.annotations.NotNull;
+
+public class BlastFurnaceRecipe extends BlastingRecipe {
+
+    private boolean registered = false;
+    private final NamespacedKey key;
+
+    public BlastFurnaceRecipe(@NotNull NamespacedKey key, @NotNull ItemStack result, @NotNull Material source, float experience, int cookingTime) {
+        super(key, result, source, experience, cookingTime);
+        this.key = key;
+    }
+
+    public BlastFurnaceRecipe(@NotNull NamespacedKey key, @NotNull ItemStack result, @NotNull ItemStack source, float experience, int cookingTime) {
+        super(key, result, new RecipeChoice.ExactChoice(source), experience, cookingTime);
+        this.key = key;
+    }
+
+    public boolean register() {
+        if (registered) {
+            return false;
+        }
+        if (Bukkit.addRecipe(this)) {
+            registered = true;
+            return true;
+        }
+        return false;
+    }
+
+    public boolean unregister() {
+        if (!registered) {
+            return false;
+        }
+        if (Bukkit.removeRecipe(this.key, true)) {
+            registered = false;
+            return true;
+        }
+        return false;
+    }
+
+}
