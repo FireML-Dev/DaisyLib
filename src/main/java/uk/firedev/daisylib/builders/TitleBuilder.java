7f2fc2239b25141db600730cc8ba908e0e9a3a8d
FireML <106587317+FireML@users.noreply.github.com>
2024-02-27 10:31:31 +0000

    Fix TitleBuilder issue
    Providing 0 would break the titles, so we make sure it isn't 0 or less.
    
    Also makes sure Title and Subtitle are not null

--- src/main/java/uk/firedev/daisylib/builders/TitleBuilder.java
+++ src/main/java/uk/firedev/daisylib/builders/TitleBuilder.java
@@ -4,6 +4,7 @@ import net.kyori.adventure.text.Component;
 import net.kyori.adventure.title.Title;
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
+import org.jetbrains.annotations.NotNull;
 import uk.firedev.daisylib.utils.ComponentUtils;
 
 import java.time.Duration;
@@ -14,42 +15,77 @@ public class TitleBuilder {
     private int in = 20;
     private int stay = 60;
     private int out = 20;
-    private Component title = null;
-    private Component subtitle = null;
+    private Component title = Component.empty();
+    private Component subtitle = Component.empty();
 
-    public TitleBuilder withTimings(int in, int stay, int out) { return withIn(in).withStay(stay).withOut(out); }
+    public TitleBuilder withTimings(int inTicks, int stayTicks, int outTicks) { return withIn(inTicks).withStay(stayTicks).withOut(outTicks); }
 
-    public TitleBuilder withIn(int in) {
-        this.in = in;
+    public TitleBuilder withTimingsSeconds(int inSeconds, int staySeconds, int outSeconds) { return withInSeconds(inSeconds).withStaySeconds(staySeconds).withOutSeconds(outSeconds); }
+
+    public TitleBuilder withIn(int inTicks) {
+        if (inTicks <= 0) {
+            return this;
+        }
+        this.in = inTicks;
         return this;
     }
 
-    public TitleBuilder withStay(int stay) {
-        this.stay = stay;
+    public TitleBuilder withInSeconds(int inSeconds) {
+        if (inSeconds <= 0) {
+            return this;
+        }
+        this.in = inSeconds * 20;
         return this;
     }
 
-    public TitleBuilder withOut(int out) {
-        this.out = out;
+    public TitleBuilder withStay(int stayTicks) {
+        if (stayTicks <= 0) {
+            return this;
+        }
+        this.stay = stayTicks;
         return this;
     }
 
-    public TitleBuilder withTitle(Component title) {
+    public TitleBuilder withStaySeconds(int staySeconds) {
+        if (staySeconds <= 0) {
+            return this;
+        }
+        this.stay = staySeconds * 20;
+        return this;
+    }
+
+    public TitleBuilder withOut(int outTicks) {
+        if (outTicks <= 0) {
+            return this;
+        }
+        this.out = outTicks;
+        return this;
+    }
+
+    public TitleBuilder withOutSeconds(int outSeconds) {
+        if (outSeconds <= 0) {
+            return this;
+        }
+        this.stay = outSeconds * 20;
+        return this;
+    }
+
+    public TitleBuilder withTitle(@NotNull Component title) {
         this.title = title;
         return this;
     }
 
-    public TitleBuilder withStringTitle(String title) {
+    public TitleBuilder withStringTitle(@NotNull String title) {
         this.title = ComponentUtils.parseComponent(title);
         return this;
     }
 
-    public TitleBuilder withSubtitle(Component subtitle) {
+    public TitleBuilder withSubtitle(@NotNull Component subtitle) {
         this.subtitle = subtitle;
         return this;
     }
 
-    public TitleBuilder withStringSubtitle(String subtitle) {
+    public TitleBuilder withStringSubtitle(@NotNull String subtitle) {
         this.subtitle = ComponentUtils.parseComponent(subtitle);
         return this;
     }
@@ -57,23 +93,19 @@ public class TitleBuilder {
     public void sendAll(String... replacements) { Bukkit.getOnlinePlayers().forEach(player -> send(player, replacements)); }
 
     public void send(Player p, String... replacements) {
-        if (this.title != null && this.subtitle != null) {
-            Component title = ComponentUtils.parsePlaceholders(this.title, replacements);
-            Component subtitle = ComponentUtils.parsePlaceholders(this.subtitle, replacements);
-            Title.Times times = Title.Times.times(Duration.ofSeconds(in / 20), Duration.ofSeconds(stay / 20), Duration.ofSeconds(out / 20));
-            p.showTitle(Title.title(title, subtitle, times));
-        }
+        Component title = ComponentUtils.parsePlaceholders(this.title, replacements);
+        Component subtitle = ComponentUtils.parsePlaceholders(this.subtitle, replacements);
+        Title.Times times = Title.Times.times(Duration.ofSeconds(in / 20), Duration.ofSeconds(stay / 20), Duration.ofSeconds(out / 20));
+        p.showTitle(Title.title(title, subtitle, times));
     }
 
     public void sendAll(Map<String, Component> replacements) { Bukkit.getOnlinePlayers().forEach(player -> send(player, replacements)); }
 
     public void send(Player p, Map<String, Component> replacements) {
-        if (title != null && subtitle != null) {
-            Component title = ComponentUtils.parsePlaceholders(this.title, replacements);
-            Component subtitle = ComponentUtils.parsePlaceholders(this.subtitle, replacements);
-            Title.Times times = Title.Times.times(Duration.ofSeconds(in / 20), Duration.ofSeconds(stay / 20), Duration.ofSeconds(out / 20));
-            p.showTitle(Title.title(title, subtitle, times));
-        }
+        Component title = ComponentUtils.parsePlaceholders(this.title, replacements);
+        Component subtitle = ComponentUtils.parsePlaceholders(this.subtitle, replacements);
+        Title.Times times = Title.Times.times(Duration.ofSeconds(in / 20), Duration.ofSeconds(stay / 20), Duration.ofSeconds(out / 20));
+        p.showTitle(Title.title(title, subtitle, times));
     }
 
 }
