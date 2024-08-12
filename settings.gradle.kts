788aed16d1fa99ffbfaacf98a081faa05d8ee504
FireML <firemlyt@gmail.com>
2024-08-12 13:23:38 +0100

    Update CommandAPI for 1.21.1 support

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -10,10 +10,10 @@ dependencyResolutionManagement {
             library("universalscheduler", "com.github.Anon8281:UniversalScheduler:0.1.6")
             library("placeholderapi", "me.clip:placeholderapi:2.11.6")
             library("vault", "com.github.MilkBowl:VaultAPI:1.7.1")
-            library("commandapi", "dev.jorel:commandapi-bukkit-shade:9.5.1")
+            library("commandapi", "dev.jorel:commandapi-bukkit-shade:9.5.3")
             library("vanishchecker", "uk.firedev:VanishChecker:1.1.0")
             library("miniplaceholders", "io.github.miniplaceholders:miniplaceholders-api:2.2.3")
-            library("boostedyaml", "dev.dejvokep:boosted-yaml:1.3.6")
+            library("boostedyaml", "dev.dejvokep:boosted-yaml:1.3.7")
 
             plugin("shadow", "com.gradleup.shadow").version("8.3.0")
             //plugin("paperweight", "io.papermc.paperweight.userdev").version("1.7.1")
