425ca9410591524b6a74d826d12d506340b5e3ac
FireML <firemlyt@gmail.com>
2024-08-17 17:30:05 +0100

    Cleanup

--- src/main/java/uk/firedev/daisylib/utils/TimerScheduling.java
+++ src/main/java/uk/firedev/daisylib/utils/TimerScheduling.java
@@ -10,24 +10,17 @@ public class TimerScheduling {
     public Timer timer = new Timer();
     public boolean started = false;
 
-    private @NotNull TimerTask task;
     private int hour;
     private int minute;
     private TimeZone timeZone;
 
-    public TimerScheduling(@NotNull TimerTask task, int hour, int minute, @Nullable TimeZone timeZone) {
-        this.task = task;
+    public TimerScheduling(int hour, int minute, @Nullable TimeZone timeZone) {
         this.hour = hour;
         this.minute = minute;
         this.timeZone = timeZone;
     }
 
-    public void reload() {
-        stop();
-        start();
-    }
-
-    public void start() {
+    public void start(@NotNull TimerTask task) {
         if (started) {
             return;
         }
@@ -45,20 +38,20 @@ public class TimerScheduling {
     }
 
     public Date getNextRun(int hour, int minute, TimeZone timeZone) {
-        Calendar next = Calendar.getInstance();
+        Calendar nextUpkeep = Calendar.getInstance();
         if (timeZone == null) {
-            next.setTimeZone(TimeZone.getDefault());
+            nextUpkeep.setTimeZone(TimeZone.getDefault());
         } else {
-            next.setTimeZone(timeZone);
+            nextUpkeep.setTimeZone(timeZone);
         }
-        if (next.get(Calendar.HOUR_OF_DAY) >= hour) {
-            next.set(Calendar.DAY_OF_YEAR, next.get(Calendar.DAY_OF_YEAR) + 1);
+        if (nextUpkeep.get(Calendar.HOUR_OF_DAY) >= hour) {
+            nextUpkeep.set(Calendar.DAY_OF_YEAR, nextUpkeep.get(Calendar.DAY_OF_YEAR) + 1);
         }
-        next.set(Calendar.HOUR_OF_DAY, hour);
-        next.set(Calendar.MINUTE, minute);
-        next.set(Calendar.SECOND, 0);
-        next.set(Calendar.MILLISECOND, 1);
-        return next.getTime();
+        nextUpkeep.set(Calendar.HOUR_OF_DAY, hour);
+        nextUpkeep.set(Calendar.MINUTE, minute);
+        nextUpkeep.set(Calendar.SECOND, 0);
+        nextUpkeep.set(Calendar.MILLISECOND, 1);
+        return nextUpkeep.getTime();
     }
 
     public void setHour(int hour) {
@@ -72,5 +65,5 @@ public class TimerScheduling {
     public void setTimeZone(TimeZone timeZone) {
         this.timeZone = timeZone;
     }
-
+    
 }
