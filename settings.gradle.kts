b8e0daaa3642dbe6b7adf0dc773a6e9c318ad6e3
FireML <firemlyt@gmail.com>
2024-08-20 22:37:33 +0100

    VanishChecker 1.0.1

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -11,7 +11,7 @@ dependencyResolutionManagement {
             library("placeholderapi", "me.clip:placeholderapi:2.11.6")
             library("vault", "com.github.MilkBowl:VaultAPI:1.7.1")
             library("commandapi", "dev.jorel:commandapi-bukkit-shade-mojang-mapped:9.5.3")
-            library("vanishchecker", "uk.firedev:VanishChecker:1.1.0")
+            library("vanishchecker", "uk.firedev:VanishChecker:1.0.1")
             library("miniplaceholders", "io.github.miniplaceholders:miniplaceholders-api:2.2.3")
             library("boostedyaml", "dev.dejvokep:boosted-yaml:1.3.7")
 
