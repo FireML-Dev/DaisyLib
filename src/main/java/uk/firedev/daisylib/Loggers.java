0c959a15a782465729a6c1df37a40dfa599c4d83
FireML <firemlyt@gmail.com>
2024-08-22 22:15:55 +0100

    Require non-null values in the Loggers class

--- src/main/java/uk/firedev/daisylib/Loggers.java
+++ src/main/java/uk/firedev/daisylib/Loggers.java
@@ -2,6 +2,7 @@ package uk.firedev.daisylib;
 
 import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.logger.slf4j.ComponentLogger;
+import org.jetbrains.annotations.NotNull;
 import uk.firedev.daisylib.message.component.ComponentMessage;
 
 import java.util.logging.Level;
@@ -11,94 +12,94 @@ public class Loggers {
     
     // net.kyori.adventure.text.logger.slf4j.ComponentLogger methods
     
-    public static void logException(ComponentLogger logger, Throwable throwable) {
+    public static void logException(@NotNull ComponentLogger logger, @NotNull Throwable throwable) {
         logger.error(throwable.getMessage(), throwable);
     }
 
-    public static void info(ComponentLogger logger, Component message) {
+    public static void info(@NotNull ComponentLogger logger, @NotNull Component message) {
         logger.info(message);
     }
 
-    public static void info(ComponentLogger logger, Component message, Throwable throwable) {
+    public static void info(@NotNull ComponentLogger logger, @NotNull Component message, @NotNull Throwable throwable) {
         info(logger, message);
         logException(logger, throwable);
     }
 
 
-    public static void info(ComponentLogger logger, String message) {
         logger.info(new ComponentMessage(message).getMessage());
+    public static void info(@NotNull ComponentLogger logger, @NotNull String message) {
     }
 
-    public static void info(ComponentLogger logger, String message, Throwable throwable) {
+    public static void info(@NotNull ComponentLogger logger, @NotNull String message, @NotNull Throwable throwable) {
         info(logger, message);
         logException(logger, throwable);
     }
-    
-    public static void warn(ComponentLogger logger, Component message) {
+
+    public static void warn(@NotNull ComponentLogger logger, @NotNull Component message) {
         logger.warn(message);
     }
 
-    public static void warn(ComponentLogger logger, Component message, Throwable throwable) {
+    public static void warn(@NotNull ComponentLogger logger, @NotNull Component message, @NotNull Throwable throwable) {
         warn(logger, message);
         logException(logger, throwable);
     }
 
-    public static void warn(ComponentLogger logger, String message) {
         logger.warn(new ComponentMessage(message).getMessage());
+    public static void warn(@NotNull ComponentLogger logger, @NotNull String message) {
     }
 
-    public static void warn(ComponentLogger logger, String message, Throwable throwable) {
+    public static void warn(@NotNull ComponentLogger logger, @NotNull String message, @NotNull Throwable throwable) {
         warn(logger, message);
         logException(logger, throwable);
     }
-    
-    public static void error(ComponentLogger logger, Component message) {
+
+    public static void error(@NotNull ComponentLogger logger, @NotNull Component message) {
         logger.error(message);
     }
 
-    public static void error(ComponentLogger logger, Component message, Throwable throwable) {
+    public static void error(@NotNull ComponentLogger logger, @NotNull Component message, @NotNull Throwable throwable) {
         error(logger, message);
         logException(logger, throwable);
     }
 
-    public static void error(ComponentLogger logger, String message) {
         logger.error(new ComponentMessage(message).getMessage());
+    public static void error(@NotNull ComponentLogger logger, @NotNull String message) {
     }
 
-    public static void error(ComponentLogger logger, String message, Throwable throwable) {
+    public static void error(@NotNull ComponentLogger logger, @NotNull String message, @NotNull Throwable throwable) {
         error(logger, message);
         logException(logger, throwable);
     }
-    
+
     // java.util.logging.Logger methods
 
-    public static void logException(Logger logger, Throwable throwable) {
+    public static void logException(@NotNull Logger logger, @NotNull Throwable throwable) {
         logger.log(Level.SEVERE, throwable.getMessage(), throwable);
     }
 
-    public static void info(Logger logger, String message) {
+    public static void info(@NotNull Logger logger, @NotNull String message) {
         logger.info(message);
     }
 
-    public static void info(Logger logger, String message, Throwable throwable) {
+    public static void info(@NotNull Logger logger, @NotNull String message, @NotNull Throwable throwable) {
         info(logger, message);
         logException(logger, throwable);
     }
 
-    public static void warn(Logger logger, String message) {
+    public static void warn(@NotNull Logger logger, @NotNull String message) {
         logger.warning(message);
     }
 
-    public static void warn(Logger logger, String message, Throwable throwable) {
+    public static void warn(@NotNull Logger logger, @NotNull String message, @NotNull Throwable throwable) {
         warn(logger, message);
         logException(logger, throwable);
     }
 
-    public static void error(Logger logger, String message) {
+    public static void error(@NotNull Logger logger, @NotNull String message) {
         logger.severe(message);
     }
 
-    public static void error(Logger logger, String message, Throwable throwable) {
+    public static void error(@NotNull Logger logger, @NotNull String message, @NotNull Throwable throwable) {
         error(logger, message);
         logException(logger, throwable);
     }
