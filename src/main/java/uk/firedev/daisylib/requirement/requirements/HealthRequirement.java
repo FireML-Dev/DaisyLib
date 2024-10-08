0c4cf392599e2c2535d4ce3516d40a87f6fae1ab
FireML <firemlyt@gmail.com>
2024-10-08 23:50:03 +0100

    Rework the Requirements to accept a List

--- src/main/java/uk/firedev/daisylib/requirement/requirements/HealthRequirement.java
+++ src/main/java/uk/firedev/daisylib/requirement/requirements/HealthRequirement.java
@@ -3,19 +3,34 @@ package uk.firedev.daisylib.requirement.requirements;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.Plugin;
 import org.jetbrains.annotations.NotNull;
+import uk.firedev.daisylib.Loggers;
 import uk.firedev.daisylib.local.DaisyLib;
+import uk.firedev.daisylib.requirement.RequirementData;
 import uk.firedev.daisylib.requirement.RequirementType;
 import uk.firedev.daisylib.utils.ObjectUtils;
+
+import java.util.List;
 
 public class HealthRequirement implements RequirementType {
 
     @Override
-    public boolean checkRequirement(@NotNull Player player, @NotNull String value) {
-        if (!ObjectUtils.isDouble(value)) {
+    public boolean checkRequirement(@NotNull RequirementData data, @NotNull List<String> values) {
+        Player player = data.getPlayer();
+        if (player == null) {
             return false;
         }
-        double healthNeeded = Double.parseDouble(value);
-        return player.getHealth() >= healthNeeded;
+        double currentHealth = player.getHealth();
+        for (String value : values) {
+            if (!ObjectUtils.isDouble(value)) {
+                Loggers.warn(getComponentLogger(), value + " is not a valid double");
+                continue;
+            }
+            double healthNeeded = Double.parseDouble(value);
+            if (currentHealth >= healthNeeded) {
+                return true;
+            }
+        }
+        return false;
     }
 
     @Override
