c28fdbc7ec9436138c0b4053cf4e40aff54beeb3
FireML <fireml@firedev.uk>
2024-12-08 01:16:18 +0000

    Update VanishChecker to 1.0.4

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -12,7 +12,7 @@ dependencyResolutionManagement {
             library("placeholderapi", "me.clip:placeholderapi:2.11.6")
             library("vault", "com.github.MilkBowl:VaultAPI:1.7.1")
             library("commandapi", "dev.jorel:commandapi-bukkit-shade:9.6.1")
-            library("vanishchecker", "uk.firedev:VanishChecker:1.0.1")
+            library("vanishchecker", "uk.firedev:VanishChecker:1.0.4")
             library("miniplaceholders", "io.github.miniplaceholders:miniplaceholders-api:2.2.3")
             library("boostedyaml", "dev.dejvokep:boosted-yaml:1.3.7")
             // TODO update this to 2.0.1 after my PR is merged
