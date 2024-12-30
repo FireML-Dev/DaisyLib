ff9534169abe0d6a17c96b67e74d7d6d2fc6c969
FireML <fireml@firedev.uk>
2024-12-30 19:50:16 +0000

    Fix ActionListener

--- src/main/java/uk/firedev/daisylib/actions/ActionListener.java
+++ src/main/java/uk/firedev/daisylib/actions/ActionListener.java
@@ -5,7 +5,7 @@ import org.jetbrains.annotations.NotNull;
 
 public abstract class ActionListener {
 
-    abstract void accept(@NotNull ActionContext context);
+    public abstract void accept(@NotNull ActionContext context);
 
     public boolean register(@NotNull String actionName) {
         return ActionManager.getInstance().registerListener(actionName, this);
