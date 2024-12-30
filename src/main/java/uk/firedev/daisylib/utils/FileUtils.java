488036a37276f37f50b09842d05c91bc6d8e438a
FireML <fireml@firedev.uk>
2024-12-30 20:11:22 +0000

    Update FileUtils

--- src/main/java/uk/firedev/daisylib/utils/FileUtils.java
+++ src/main/java/uk/firedev/daisylib/utils/FileUtils.java
@@ -3,12 +3,16 @@ package uk.firedev.daisylib.utils;
 import org.bukkit.plugin.Plugin;
 import org.jetbrains.annotations.NotNull;
 import uk.firedev.daisylib.Loggers;
+import uk.firedev.daisylib.local.DaisyLib;
 
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.nio.file.Files;
 import java.nio.file.StandardCopyOption;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Level;
 
 public class FileUtils {
 
@@ -55,6 +59,53 @@ public class FileUtils {
             return directory.mkdirs();
         }
         return true;
+    }
+
+    /**
+     * Retrieves all files in the given directory.
+     *
+     * @param directory The directory to search
+     * @param ignoreUnderscoreFiles Should files that start with an underscore be ignored?
+     * @param recursive Should this also search subdirectories?
+     * @return A list of files in the directory. Returns an empty list if none.
+     */
+    public static List<File> getFilesInDirectory(@NotNull File directory, boolean ignoreUnderscoreFiles, boolean recursive) {
+        List<File> finalList = new ArrayList<>();
+        if (!directory.exists() || !directory.isDirectory()) {
+            return finalList;
+        }
+        try {
+            File[] fileArray = directory.listFiles();
+            if (fileArray == null) {
+                return finalList;
+            }
+            for (File file : fileArray) {
+                if (ignoreUnderscoreFiles && file.getName().startsWith("_")) {
+                    continue;
+                }
+                if (file.isDirectory() && recursive) {
+                    finalList.addAll(getFilesInDirectory(file, ignoreUnderscoreFiles, true));
+                } else if (file.isFile()) {
+                    finalList.add(file);
+                }
+            }
+        } catch (SecurityException exception) {
+            Loggers.warn(
+                    DaisyLib.getInstance().getComponentLogger(),
+                    "Failed to retrieve files in " + directory.getAbsolutePath() + ": Access Denied.",
+                    exception
+            );
+        }
+        return finalList;
+    }
+
+    public static boolean doesDirectoryContainFile(@NotNull File directory, @NotNull String fileName, boolean recursive) {
+        for (File file : getFilesInDirectory(directory, false, recursive)) {
+            if (file.getName().equals(fileName)) {
+                return true;
+            }
+        }
+        return false;
     }
 
 }
