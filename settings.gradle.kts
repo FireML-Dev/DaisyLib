21bc19a674b8599ff6d4b4d797fbb3939507ad7b
FireML <fireml@firedev.uk>
2025-01-18 09:41:29 +0000

    Use mojang mapped CommandAPI

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -10,7 +10,7 @@ dependencyResolutionManagement {
 
             library("placeholderapi", "me.clip:placeholderapi:2.11.6")
             library("vault", "com.github.MilkBowl:VaultAPI:1.7")
-            library("commandapi", "dev.jorel:commandapi-bukkit-shade:9.7.0")
+            library("commandapi", "dev.jorel:commandapi-bukkit-shade-mojang-mapped:9.7.0")
             library("vanishchecker", "uk.firedev:VanishChecker:1.0.4")
             library("miniplaceholders", "io.github.miniplaceholders:miniplaceholders-api:2.2.3")
             library("boostedyaml", "dev.dejvokep:boosted-yaml:1.3.7")
