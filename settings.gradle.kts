1f28575b9584eee4c3398e4e46da138f65462085
FireML <fireml@firedev.uk>
2025-06-25 22:03:59 +0100

    Update CommandAPI

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -17,7 +17,7 @@ dependencyResolutionManagement {
             // implementation dependencies
             library("triumphgui", "dev.triumphteam:triumph-gui:3.1.11")
             library("anvilgui", "de.md5lukas:anvilgui:2.0.0")
-            library("commandapi", "dev.jorel:commandapi-bukkit-shade-mojang-mapped:10.0.1")
+            library("commandapi", "dev.jorel:commandapi-bukkit-shade-mojang-mapped:10.1.0")
             library("vanishchecker", "uk.firedev:VanishChecker:1.0.5")
             library("boostedyaml", "dev.dejvokep:boosted-yaml:1.3.7")
             library("bstats", "org.bstats:bstats-bukkit:3.1.0")
