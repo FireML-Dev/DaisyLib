561638dfe0dd5cbceb016a62c0b239148ee89c4b
FireML <fireml@firedev.uk>
2025-09-27 03:41:48 +0100

    Update MessageLib to 1.0.1

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -18,7 +18,7 @@ dependencyResolutionManagement {
             library("boostedyaml", "dev.dejvokep:boosted-yaml:1.3.7")
             library("bstats", "org.bstats:bstats-bukkit:3.1.0")
             library("customblockdata", "com.jeff-media:custom-block-data:2.2.4")
-            library("messagelib", "uk.firedev:MessageLib:1.0-SNAPSHOT")
+            library("messagelib", "uk.firedev:MessageLib:1.0.1")
 
             // Loaded via Paper's library loader
             library("nashorn", "org.openjdk.nashorn:nashorn-core:15.6")
