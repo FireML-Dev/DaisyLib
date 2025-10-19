aeae8ba536294a169be65c2007c6c99618065437
FireML <fireml@firedev.uk>
2025-10-20 00:36:46 +0100

    Add ConfigurationSection methods to Requirement

--- src/main/java/uk/firedev/daisylib/addons/requirement/Requirement.java
+++ src/main/java/uk/firedev/daisylib/addons/requirement/Requirement.java
@@ -1,10 +1,14 @@
 package uk.firedev.daisylib.addons.requirement;
 
+import dev.dejvokep.boostedyaml.block.implementation.Section;
 import net.kyori.adventure.text.logger.slf4j.ComponentLogger;
+import org.bukkit.configuration.ConfigurationSection;
 import org.bukkit.plugin.Plugin;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 import uk.firedev.daisylib.Loggers;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -12,24 +16,26 @@ import java.util.Map;
 public class Requirement {
 
     private final ComponentLogger logger = ComponentLogger.logger(getClass());
-    private final Map<String, List<String>> checkMap;
+    private final Map<String, List<String>> checkMap = new HashMap<>();
     private final Plugin plugin;
 
     public Requirement(@NotNull Plugin plugin) {
-        checkMap = new HashMap<>();
         this.plugin = plugin;
     }
 
     public Requirement(@NotNull String identifier, @NotNull List<String> values, @NotNull Plugin plugin) {
-        this.plugin = plugin;
-        checkMap = new HashMap<>();
-        processRequirement(identifier, values);
+        this(plugin);
+        add(identifier, values);
     }
 
     public Requirement(@NotNull Map<String, List<String>> requirements, @NotNull Plugin plugin) {
-        this.plugin = plugin;
-        checkMap = new HashMap<>();
-        requirements.forEach(this::processRequirement);
+        this(plugin);
+        add(requirements);
+    }
+
+    public Requirement(@Nullable ConfigurationSection section, @NotNull Plugin plugin) {
+        this(plugin);
+        add(section);
     }
 
     public Requirement add(@NotNull String identifier, @NotNull List<String> values) {
@@ -42,11 +48,30 @@ public class Requirement {
         return this;
     }
 
+    public Requirement add(@Nullable ConfigurationSection section) {
+        if (section == null) {
+            return this;
+        }
+        section.getKeys(false).forEach(requirementString -> {
+            List<String> values = new ArrayList<>();
+            if (section.isList(requirementString)) {
+                values.addAll(section.getStringList(requirementString));
+            } else {
+                values.add(section.getString(requirementString));
+            }
+            processRequirement(requirementString, values);
+        });
+        return this;
+    }
+
     private void processRequirement(@NotNull String identifier, @NotNull List<String> values) {
         this.checkMap.put(identifier, values);
     }
 
     public boolean meetsRequirements(@NotNull RequirementData data) {
+        if (checkMap.isEmpty()) {
+            return true;
+        }
         for (Map.Entry<String, List<String>> entry : checkMap.entrySet()) {
             String key = entry.getKey().toUpperCase();
             List<String> value = entry.getValue();
