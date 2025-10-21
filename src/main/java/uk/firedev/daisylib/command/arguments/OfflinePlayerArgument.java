c68bfd1302164e75b08db4a52cd44fc2f1efe41a
FireML <fireml@firedev.uk>
2025-10-21 21:08:20 +0100

    Create an abstract wrapper for CustomArgumentType.Converted

--- src/main/java/uk/firedev/daisylib/command/arguments/OfflinePlayerArgument.java
+++ src/main/java/uk/firedev/daisylib/command/arguments/OfflinePlayerArgument.java
@@ -1,41 +1,83 @@
 package uk.firedev.daisylib.command.arguments;
 
+import com.mojang.brigadier.arguments.ArgumentType;
+import com.mojang.brigadier.arguments.StringArgumentType;
+import com.mojang.brigadier.exceptions.CommandSyntaxException;
+import com.mojang.brigadier.exceptions.DynamicCommandExceptionType;
 import dev.jorel.commandapi.arguments.Argument;
 import dev.jorel.commandapi.arguments.CustomArgument;
 import dev.jorel.commandapi.arguments.StringArgument;
+import io.papermc.paper.command.brigadier.MessageComponentSerializer;
+import net.kyori.adventure.text.Component;
 import org.bukkit.Bukkit;
 import org.bukkit.OfflinePlayer;
 import org.bukkit.entity.Player;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+import uk.firedev.daisylib.command.ArgumentBase;
 import uk.firedev.daisylib.utils.PlayerHelper;
 import uk.firedev.daisylib.command.ArgumentBuilder;
 
+import java.util.List;
 import java.util.function.Predicate;
+import java.util.stream.Stream;
 
-public class OfflinePlayerArgument {
+public class OfflinePlayerArgument extends ArgumentBase<OfflinePlayer, String> {
 
-    public static Argument<OfflinePlayer> create(@NotNull String nodeName) {
-        return create(nodeName, offlinePlayer -> true);
+    private static final DynamicCommandExceptionType CANNOT_USE = new DynamicCommandExceptionType(val ->
+        MessageComponentSerializer.message().serialize(Component.text("This player cannot be used: " + val))
+    );
+
+    private final Predicate<OfflinePlayer> predicate;
+
+    private OfflinePlayerArgument(@NotNull Predicate<OfflinePlayer> predicate) {
+        this.predicate = predicate;
     }
 
-    public static Argument<OfflinePlayer> create(@NotNull String nodeName, @NotNull Predicate<OfflinePlayer> predicate) {
-        return new CustomArgument<>(new StringArgument(nodeName), info -> {
-            OfflinePlayer player = Bukkit.getOfflinePlayer(info.input());
-            if (!predicate.test(player)) {
-                throw CustomArgument.CustomArgumentException.fromMessageBuilder(
-                    new CustomArgument.MessageBuilder("This player cannot be used: ").appendArgInput()
-                );
-            }
-            return player;
-        }).includeSuggestions(
-            ArgumentBuilder.getAsyncSuggestions(info ->
-                Bukkit.getOnlinePlayers().stream().map(Player::getName).toArray(String[]::new)
-            )
-        );
+    public static OfflinePlayerArgument create() {
+        return create(offlinePlayer -> true);
     }
 
-    public static Argument<OfflinePlayer> createPlayedBefore(@NotNull String nodeName) {
-        return create(nodeName, PlayerHelper::hasPlayerBeenOnServer);
+    public static OfflinePlayerArgument create(@NotNull Predicate<OfflinePlayer> predicate) {
+        return new OfflinePlayerArgument(predicate);
+    }
+
+    public static OfflinePlayerArgument createPlayedBefore() {
+        return create(PlayerHelper::hasPlayerBeenOnServer);
+    }
+
+    @Override
+    public List<String> getSuggestions() {
+        return Bukkit.getOnlinePlayers().stream().filter(this.predicate).map(Player::getName).toList();
+    }
+
+    /**
+     * Converts the value from the native type to the custom argument type.
+     *
+     * @param nativeType native argument provided value
+     * @return converted value
+     * @throws CommandSyntaxException if an exception occurs while parsing
+     * @see #convert(Object, Object)
+     */
+    @Override
+    public OfflinePlayer convert(String nativeType) throws CommandSyntaxException {
+        OfflinePlayer player = Bukkit.getOfflinePlayer(nativeType);
+        if (!predicate.test(player)) {
+            throw CANNOT_USE.create(nativeType);
+        }
+        return player;
+    }
+
+    /**
+     * Gets the native type that this argument uses,
+     * the type that is sent to the client.
+     *
+     * @return native argument type
+     */
+    @NotNull
+    @Override
+    public ArgumentType<String> getNativeType() {
+        return StringArgumentType.string();
     }
 
 }
--- src/main/java/uk/firedev/daisylib/command/arguments/PlayerArgument.java
+++ src/main/java/uk/firedev/daisylib/command/arguments/PlayerArgument.java
@@ -1,42 +1,78 @@
 package uk.firedev.daisylib.command.arguments;
 
-import dev.jorel.commandapi.arguments.Argument;
-import dev.jorel.commandapi.arguments.CustomArgument;
-import dev.jorel.commandapi.arguments.StringArgument;
+import com.mojang.brigadier.arguments.ArgumentType;
+import com.mojang.brigadier.arguments.StringArgumentType;
+import com.mojang.brigadier.exceptions.CommandSyntaxException;
+import com.mojang.brigadier.exceptions.DynamicCommandExceptionType;
+import io.papermc.paper.command.brigadier.MessageComponentSerializer;
+import net.kyori.adventure.text.Component;
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import org.jetbrains.annotations.NotNull;
-import uk.firedev.daisylib.command.ArgumentBuilder;
+import uk.firedev.daisylib.command.ArgumentBase;
+import uk.firedev.daisylib.utils.PlayerHelper;
 
+import java.util.List;
 import java.util.function.Predicate;
 
-public class PlayerArgument {
+public class PlayerArgument extends ArgumentBase<Player, String> {
 
-    public static Argument<Player> create(@NotNull String nodeName) {
-        return create(nodeName, player -> true);
+    private static final DynamicCommandExceptionType INVALID_PLAYER = new DynamicCommandExceptionType(val ->
+        MessageComponentSerializer.message().serialize(Component.text("This player is not online: " + val))
+    );
+    private static final DynamicCommandExceptionType CANNOT_USE = new DynamicCommandExceptionType(val ->
+        MessageComponentSerializer.message().serialize(Component.text("This player cannot be used: " + val))
+    );
+
+    private final Predicate<Player> predicate;
+
+    private PlayerArgument(@NotNull Predicate<Player> predicate) {
+        this.predicate = predicate;
     }
 
-    public static Argument<Player> create(@NotNull String nodeName, @NotNull Predicate<Player> predicate) {
-        return new CustomArgument<>(
-            new StringArgument(nodeName), info -> {
-            Player player = Bukkit.getPlayer(info.input());
-            if (player == null) {
-                throw CustomArgument.CustomArgumentException.fromMessageBuilder(
-                    new CustomArgument.MessageBuilder("Player is not online: ").appendArgInput()
-                );
-            }
-            if (!predicate.test(player)) {
-                throw CustomArgument.CustomArgumentException.fromMessageBuilder(
-                    new CustomArgument.MessageBuilder("This player cannot be used: ").appendArgInput()
-                );
-            }
-            return player;
+    public static PlayerArgument create() {
+        return create(Player -> true);
+    }
+
+    public static PlayerArgument create(@NotNull Predicate<Player> predicate) {
+        return new PlayerArgument(predicate);
+    }
+
+    @Override
+    public List<String> getSuggestions() {
+        return Bukkit.getOnlinePlayers().stream().filter(this.predicate).map(Player::getName).toList();
+    }
+
+    /**
+     * Converts the value from the native type to the custom argument type.
+     *
+     * @param nativeType native argument provided value
+     * @return converted value
+     * @throws CommandSyntaxException if an exception occurs while parsing
+     * @see #convert(Object, Object)
+     */
+    @Override
+    public Player convert(String nativeType) throws CommandSyntaxException {
+        Player player = Bukkit.getPlayer(nativeType);
+        if (player == null) {
+            throw INVALID_PLAYER.create(nativeType);
         }
-        ).includeSuggestions(
-            ArgumentBuilder.getAsyncSuggestions(info ->
-                Bukkit.getOnlinePlayers().stream().map(Player::getName).toArray(String[]::new)
-            )
-        );
+        if (!predicate.test(player)) {
+            throw CANNOT_USE.create(nativeType);
+        }
+        return player;
+    }
+
+    /**
+     * Gets the native type that this argument uses,
+     * the type that is sent to the client.
+     *
+     * @return native argument type
+     */
+    @NotNull
+    @Override
+    public ArgumentType<String> getNativeType() {
+        return StringArgumentType.string();
     }
 
 }
