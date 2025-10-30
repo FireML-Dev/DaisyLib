e4d91b0a0695e95aec748bd398cd87b8ffc8a089
FireML <fireml@firedev.uk>
2025-10-30 02:56:52 +0000

    Add CommandUtils

---
+++ src/main/java/uk/firedev/daisylib/command/CommandUtils.java
@@ -0,0 +1,36 @@
+package uk.firedev.daisylib.command;
+
+import io.papermc.paper.command.brigadier.CommandSourceStack;
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.format.NamedTextColor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.function.Predicate;
+
+public class CommandUtils {
+
+    public static @Nullable Player requirePlayer(@Nullable CommandSourceStack source) {
+        if (source == null) {
+            return null;
+        }
+        CommandSender sender = source.getSender();
+        if (!(sender instanceof Player player)) {
+            sender.sendMessage(Component.text("Only players can use this command.").color(NamedTextColor.RED));
+            return null;
+        }
+        return player;
+    }
+
+    public static Predicate<CommandSourceStack> playerPredicate(@NotNull Predicate<Player> playerPredicate) {
+        return sender -> {
+            if (!(sender.getSender() instanceof Player player)) {
+                return false;
+            }
+            return playerPredicate.test(player);
+        };
+    }
+
+}
