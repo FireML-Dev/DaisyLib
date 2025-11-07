fa7859e10c80a11ebe07000389b9271550f3a088
FireML <fireml@firedev.uk>
2025-11-07 01:30:28 +0000

    Register all ActionAddons

--- src/main/java/uk/firedev/daisylib/local/DaisyLib.java
+++ src/main/java/uk/firedev/daisylib/local/DaisyLib.java
@@ -6,18 +6,9 @@ import org.bstats.bukkit.Metrics;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.jetbrains.annotations.NotNull;
 import uk.firedev.daisylib.VaultManager;
-import uk.firedev.daisylib.addons.requirement.types.ExpRequirementAddon;
-import uk.firedev.daisylib.addons.requirement.types.HealthRequirementAddon;
-import uk.firedev.daisylib.addons.requirement.types.HoldingRequirementAddon;
-import uk.firedev.daisylib.addons.requirement.types.MoneyRequirementAddon;
-import uk.firedev.daisylib.addons.requirement.types.PermissionRequirementAddon;
-import uk.firedev.daisylib.addons.requirement.types.WorldRequirementAddon;
-import uk.firedev.daisylib.addons.reward.types.CommandRewardAddon;
-import uk.firedev.daisylib.addons.reward.types.ExpRewardAddon;
-import uk.firedev.daisylib.addons.reward.types.HealthRewardAddon;
-import uk.firedev.daisylib.addons.reward.types.ItemRewardAddon;
-import uk.firedev.daisylib.addons.reward.types.MoneyRewardAddon;
-import uk.firedev.daisylib.addons.reward.types.PermissionRewardAddon;
+import uk.firedev.daisylib.addons.action.types.*;
+import uk.firedev.daisylib.addons.requirement.types.*;
+import uk.firedev.daisylib.addons.reward.types.*;
 import uk.firedev.daisylib.events.CustomEventListener;
 import uk.firedev.daisylib.events.DaisyLibReloadEvent;
 import uk.firedev.daisylib.local.command.LibCommandBrigadier;
@@ -86,6 +77,25 @@ public final class DaisyLib extends JavaPlugin {
         new MoneyRequirementAddon().register();
         new PermissionRequirementAddon().register();
         new WorldRequirementAddon().register();
+
+        // ActionAddons
+        new BreakActionAddon().register();
+        new BreedActionAddon().register();
+        new BrushActionAddon().register();
+        new BucketActionAddon().register();
+        new ConsumeActionAddon().register();
+        new DyeActionAddon().register();
+        new EnchantActionAddon().register();
+        new FishActionAddon().register();
+        new KillActionAddon().register();
+        new MilkActionAddon().register();
+        new PlaceActionAddon().register();
+        new ShearBlockActionAddon().register();
+        new ShearEntityActionAddon().register();
+        new SmeltActionAddon().register();
+        new StripActionAddon().register();
+        new TameActionAddon().register();
+        new TradeActionAddon().register();
     }
 
     public static @NotNull DaisyLib getInstance() {
