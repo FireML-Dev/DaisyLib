43d61a5dddb8f8bc6234ebaf3da5dabccb2caaa4
FireML <fireml@firedev.uk>
2026-07-30 03:33:43 +0100

    Remove ReadOnlyPair empty method
    Why did I add this if it's read-only...

--- common/src/main/java/uk/firedev/daisylib/common/pairs/ReadOnlyPair.java
+++ common/src/main/java/uk/firedev/daisylib/common/pairs/ReadOnlyPair.java
@@ -7,10 +7,6 @@ import org.jspecify.annotations.Nullable;
  */
 public record ReadOnlyPair<L, R>(@Nullable L left, @Nullable R right) {
 
-    public static <L, R> ReadOnlyPair<L, R> empty() {
-        return new ReadOnlyPair<>(null, null);
-    }
-
     public boolean isNotEmpty() {
         return !isEmpty();
     }
