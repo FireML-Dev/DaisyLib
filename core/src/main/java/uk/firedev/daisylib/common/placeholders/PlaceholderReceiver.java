e94634380bdd7eb6069e0d09eb48618c62c9d44f
FireML <fireml@firedev.uk>
2026-07-31 05:40:08 +0100

    Add PlaceholderAPI hooks

---
+++ core/src/main/java/uk/firedev/daisylib/common/placeholders/PlaceholderReceiver.java
@@ -0,0 +1,24 @@
+package uk.firedev.daisylib.common.placeholders;
+
+import me.clip.placeholderapi.expansion.PlaceholderExpansion;
+import org.bukkit.OfflinePlayer;
+import org.jspecify.annotations.NonNull;
+import org.jspecify.annotations.Nullable;
+
+import java.util.List;
+
+public abstract class PlaceholderReceiver extends PlaceholderExpansion {
+
+    public abstract @NonNull List<@NonNull IPlaceholder> getCustomPlaceholders();
+
+    @Override
+    public @Nullable String onRequest(@Nullable OfflinePlayer player, @NonNull String identifier) {
+        for (IPlaceholder placeholder : getCustomPlaceholders()) {
+            if (placeholder.shouldProcess(identifier)) {
+                return placeholder.parse(player, identifier);
+            }
+        }
+        return null;
+    }
+
+}
