4b2ebc2946273f02f4d0af4848fda0e782c8d68f
FireML <fireml@firedev.uk>
2026-08-06 18:28:32 +0100

    Add single-target player profile method

--- src/main/java/uk/firedev/daisylib/command/CommandUtils.java
+++ src/main/java/uk/firedev/daisylib/command/CommandUtils.java
@@ -15,8 +15,10 @@ import org.bukkit.entity.Player;
 import org.jspecify.annotations.NonNull;
 import org.jspecify.annotations.Nullable;
 
+import java.util.Collection;
 import java.util.List;
 import java.util.Objects;
+import java.util.UUID;
 import java.util.function.Predicate;
 
 @SuppressWarnings("UnstableApiUsage")
@@ -24,6 +26,12 @@ public class CommandUtils {
 
     private static final SimpleCommandExceptionType PLAYER_REQUIRED = new SimpleCommandExceptionType(
         new LiteralMessage("Only players can use this command.")
+    );
+    private static final SimpleCommandExceptionType ONLY_ONE_TARGET = new SimpleCommandExceptionType(
+        new LiteralMessage("Multiple targets selected when only one is required.")
+    );
+    private static final SimpleCommandExceptionType INVALID_TARGET = new SimpleCommandExceptionType(
+        new LiteralMessage("Invalid target.")
     );
 
     public static @NonNull Player requirePlayer(@Nullable CommandSourceStack source) throws CommandSyntaxException {
@@ -60,6 +68,22 @@ public class CommandUtils {
             .map(Bukkit::getOfflinePlayer)
             .filter(player -> player.getFirstPlayed() != 0)
             .toList();
+    }
+
+    public static OfflinePlayer parsePlayerProfileArgument(@NonNull CommandSourceStack stack, @NonNull PlayerProfileListResolver resolver) throws CommandSyntaxException {
+        List<PlayerProfile> profiles = List.copyOf(resolver.resolve(stack));
+        if (profiles.size() != 1) {
+            throw ONLY_ONE_TARGET.create();
+        }
+        UUID uuid = profiles.getFirst().getId();
+        if (uuid == null) {
+            throw INVALID_TARGET.create();
+        }
+        OfflinePlayer player = Bukkit.getOfflinePlayer(uuid);
+        if (player.getFirstPlayed() == 0) {
+            throw INVALID_TARGET.create();
+        }
+        return player;
     }
 
     public static List<Player> parsePlayersArgument(@NonNull CommandSourceStack stack, @NonNull PlayerSelectorArgumentResolver resolver) throws CommandSyntaxException {
